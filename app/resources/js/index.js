43e66396d40aaf1212c696afacbae558a1968bda
Jesse <Jesseokeya@gmail.com>
2018-03-31 10:15:46 -0400

    bug fix ✓

--- app/resources/js/index.js
+++ app/resources/js/index.js
@@ -1,4 +1,11 @@
 $(document).ready(() => {
   $('#modalButton').click();
   $('#modalButton').hide();
+  hideMap();
 });
+
+const hideMap = () => {
+  if(window.location.protocol !== "https:") {
+    $('#sectionMap').remove();
+  }
+}
